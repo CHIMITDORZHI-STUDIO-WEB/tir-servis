e2f6cb8c32031b150b416b224791667f251249d3
Chimitdorzhi Studio <50713661+chimitdorzhi@users.noreply.github.com>
2026-03-05 23:08:55 +0900

    Add files via upload

---
+++ Index.tsx
@@ -0,0 +1,29 @@
+import Layout from "@/components/Layout";
+import HeroSection from "@/components/HeroSection";
+import WhyUsSection from "@/components/WhyUsSection";
+import ServicesSection from "@/components/ServicesSection";
+import GallerySection from "@/components/GallerySection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+import AboutSection from "@/components/AboutSection";
+import ContactSection from "@/components/ContactSection";
+import SectionDivider from "@/components/SectionDivider";
+
+const Index = () => (
+  <Layout>
+    <HeroSection />
+    <SectionDivider />
+    <WhyUsSection />
+    <SectionDivider />
+    <ServicesSection />
+    <SectionDivider />
+    <GallerySection />
+    <SectionDivider />
+    <TestimonialsSection />
+    <SectionDivider />
+    <AboutSection />
+    <SectionDivider />
+    <ContactSection />
+  </Layout>
+);
+
+export default Index;
